74dc5558f0664c90fc453406e1a483ee0259ac6f
sur <sur00mx@gmail.com>
2021-05-26 11:48:32 -0700

    Testing bernoulli mix with vp & vq user data
    Also added version for optional new level

--- bern_mix.r
+++ bern_mix.r
@@ -29,19 +29,28 @@ process_arguments <- function(){
   args <- parse_args(p)
   
   # Process arguments
+  args$iter <- 2000
+  args$warmup <- 1000
+  args$chains <- 4
+  args$vp <- 5
+  args$vq <- 5
   
   return(args)
 }
 
-args <- process_arguments()
-# args <- list(input = "sites_dist.tsv.gz",
-#              q_thres = 0.8,
-#              min_patients = 5, 
-#              outdir = "output")
+# args <- process_arguments()
+args <- list(input = "sites_dist.tsv.gz",
+             q_thres = 0.1,
+             min_patients = 5,
+             outdir = "output",
+             iter = 3000,
+             warmup = 2000,
+             chains = 4,
+             vp = 5,
+             vq = 5)
 
 library(tidyverse)
 library(rstan)
-
 
 dat <- read_tsv(args$input,
                 col_types = cols(site_id = col_character()))
@@ -51,9 +60,11 @@ if(max(dat$n_patients) < args$min_patients){
 }
 
 # Creating site index, no need to randomly subset anymore
-# sites <- dat$site_id
-# nsites <- 10000
-# set.seed(38924)
+# subseting randomly
+nsites <- 1000
+set.seed(38924)
+dat <- dat %>%
+  filter(site_id %in% sample(unique(dat$site_id), size = nsites))
 # sites <- sample(sites, size = nsites, replace = FALSE)
 sites <- tibble(site_id = dat$site_id,
                 id = 1:length(dat$site_id))
@@ -71,44 +82,61 @@ stan_data <- dat %>%
 stan_data <- list(x = stan_data$x,
      id = stan_data$id,
      nobs = length(stan_data$x),
-     nsites = max(stan_data$id))
+     nsites = max(stan_data$id),
+     vp = args$vp,
+     vq = args$vq)
 
 cat("Compiling stan model...\n")
 # First find the location of the file via introspection
-stan_file <- commandArgs(trailingOnly = FALSE)
-i <- which(stan_file %>%
-             str_detect("--file"))
-stan_file <- stan_file[i] %>%
-  str_remove("^--file=") %>%
-  dirname()
-stan_file <- file.path(stan_file, "stan", "bernoulli_mix_multisite.stan")
+# stan_file <- commandArgs(trailingOnly = FALSE)
+# i <- which(stan_file %>%
+#              str_detect("--file"))
+# stan_file <- stan_file[i] %>%
+#   str_remove("^--file=") %>%
+#   dirname()
+# stan_file <- file.path(stan_file, "stan", "bernoulli_mix_multisite.stan")
+stan_file <- "~/micropopgen/src/hct/stan/bernoulli_mix_multisite.stan"
+# stan_file <- "~/micropopgen/src/hct/stan/bernoulli_mix_multisite_hyper.stan"
 m1.model <- stan_model(stan_file,
                        model_name = "bern_change")
 
 cat("Running Stan...\n")
 # 100 sites 8 seconds
-# 1000 sites 192 seconds
+# 1000 sites 192 seconds, 241 seconds for hyper, 500 seconds for double hyper
 # 10000 sites 4832 seconds (~1.3 hrs)
-# 100000 if continueas linearlu, ~53 hrs or  ~2.5 days
+# 100000 if trend continues linearlu, ~53 hrs or  ~2.5 days
 m1.stan <- sampling(m1.model,
                     data = stan_data,
-                    chains = 4,
-                    iter = 2000,
-                    warmup = 1000,
+                    chains = args$chains,
+                    iter = args$iter,
+                    warmup = args$warmup,
                     thin = 1,
-                    cores = 4)
-print(m1.stan, pars = c("P", "Q"))
+                    cores = args$chains)
+# load("output/m1.stan.rdat")
+print(m1.stan, pars = c("P", "Q", "vp", "vq"))
+bayesplot::mcmc_pairs(m1.stan, pars = c("P", "Q", "vp", "vq"))
+bayesplot::mcmc_trace(m1.stan, pars = c("P", "Q", "vp", "vq"))
+bayesplot::mcmc_acf(m1.stan, pars = c("P", "Q", "vp", "vq"))
+
+# mu <- 0.2
+# v <- 10
+# hist(rbeta(n = 1000, shape1 = mu * v,  shape2 = (1-mu) * v), breaks = 20)
 
 # Prepare output dir
 if(!dir.exists(args$outdir)){
   dir.create(args$outdir)
 }
 
+filename <- file.path(args$outdir, "model_summaries.tsv.gz")
+# write_tsv(summary(m1.stan)$summary %>%
+#             as.data.frame() %>%
+#             rownames_to_column(var = "var"),
+#           filename)
+
 # Save model
 cat("Saving stan model...\n")
 filename <- file.path(args$outdir, "m1.stan.rdat")
-save(m1.stan, file = filename)
-
+# save(m1.stan, file = filename)
 
 cat("Extracting posterior...\n")
 post <- rstan::extract(m1.stan)
@@ -132,7 +160,7 @@ post <- rstan::extract(m1.stan)
 
 cat("Calculating p_directional...\n")
 res <- rep(0, stan_data$nsites)
-for(iter in 1:length(post$P)){
+for(iter in 1:((args$iter - args$warmup) * args$chains)){
   # i <- 1
   # post <- matrix(1:20, nrow = 5)
   # post
@@ -148,10 +176,10 @@ res <- dat %>%
   inner_join(sites %>% left_join(res, by = "id"),
             by = "site_id")
 filename <- file.path(args$outdir, "p_directional.tsv.gz")
-write_tsv(res, filename)
+# write_tsv(res, filename)
  
-# res %>%
-#   arrange(desc(p_directional))
+res %>%
+  arrange(desc(p_directional))
 # res %>%
 #   filter(pval_inc < 0.05 & pval_dec < 0.05)
 # 
