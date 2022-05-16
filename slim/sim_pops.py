6c1a7704165a29bc1bc4c776a24518e322f246e6
sur <sur00mx@gmail.com>
2022-05-16 14:58:20 -0700

    prepare output

--- slim/sim_pops.py
+++ slim/sim_pops.py
@@ -16,6 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
+import random
 
 
 def process_arguments():
@@ -37,12 +38,20 @@ def process_arguments():
                                 "in the 5-th column."),
                           required=True,
                           type=str)
-    required.add_argument("--run_id",
-                          help="Run ID",
+    required.add_argument("--sim_id",
+                          help="Sim ID",
+                          type=str,
+                          required=True)
+    required.add_argument("--slim_script",
+                          help="SLiM script",
                           type=str,
                           required=True)
 
     # Define other arguments
+    parser.add_argument("--n_pops",
+                        help="Number of replicate populations",
+                        type=int,
+                        default=1)
     parser.add_argument("--Ne",
                         help=("Effective population size"),
                         type=int,
@@ -67,10 +76,9 @@ def process_arguments():
                         help="Average HGT tract length in bp.",
                         type=int,
                         default=1e5)
-    parser.add_argument("--rund_id_short",
+    parser.add_argument("--run_id_short",
                         help="Short run_id",
-                        type=str,
-                        default='short_id')
+                        type=str)
     parser.add_argument("--n_generations",
                         help="Number of generations.",
                         type=int,
@@ -80,18 +88,74 @@ def process_arguments():
                         type=int,
                         default=20)
     parser.add_argument("--scoef",
-                        help="Selection coefficient.",
+                        help="Selection advantage.",
                         type=float,
                         default=0.01)
-
-
-
-
+    parser.add_argument("--prop_selection",
+                        help="Proportion of de novo sites under selection.",
+                        type=float,
+                        default=0.00)
+    parser.add_argument("--sim_seed",
+                        help="Seed for simulation",
+                        type=int)
+    parser.add_argument("--print_period",
+                        help="How often to print",
+                        type=int,
+                        default=10)
+    parser.add_argument("--outdir",
+                        help="output directory",
+                        type=str,
+                        default="output")
 
     # Read arguments
     print("Reading arguments")
     args = parser.parse_args()
 
     # Processing goes here if needed
+    if args.sim_seed is None:
+        print("Creating automatic seed")
+        args.sim_seed = random.randrange(1, 10000000)
+
+    if args.run_id_short is None:
+        print("Setting run_id_short to sim_id")
+        args.run_id_short = args.sim_id
 
     return args
+
+
+if __name__ == '__main__':
+    args = process_arguments()
+    print(args)
+
+    random.seed(args.sim_seed)
+    Seeds = []
+    for pop_i in range(1, args.n_pops + 1):
+
+        seed = random.randrange(1, 10000000)
+        Seeds.append(seed)
+
+        cmd = ['slim',
+               "-t",  # print SLiM's total execution time (in user clock time)
+               "-m",  # print SLiM's peak memory usage
+               "-d", "runId='{}'" .format(args.sim_id),
+               "-d", "runIdShort='{}'" .format(args.run_id_short),
+               "-d", "info_file='{}'" .format(args.info_file),
+               "-d", "standing_variation='{}'".format(args.standing_variation),
+
+               "-d", "Ne={}".format(args.Ne),
+               "-d", "Mu={}".format(args.Mu),
+               "-d", "Rho={}".format(args.Rho),
+               "-d", "genomeSize={}".format(args.genome_size),
+               "-d", "gcBurnin={}".format(args.gcBurnin),
+               "-d", "tractlen={}".format(args.tractlen),
+               "-d", "N_generations={}".format(args.n_generations),
+               "-d", "sampleSize={}".format(args.sample_size),
+
+               "-d", "scoef1={}".format(args.scoef),
+               "-d", "prop_selection={}" .format(args.prop_selection),
+               "-d", "seed={}" .format(seed),
+               "-d", "print_period={}" .format(args.print_period),
+               args.slim_script]
+        cmd = " ".join(cmd)
+        print(cmd)
+    print(Seeds)
