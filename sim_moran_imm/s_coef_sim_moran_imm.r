56254c322d62831bcfab3f995b914834f988f34b
sur <sur00mx@gmail.com>
2022-09-07 17:23:13 -0700

    using HMVAR::s_coefficient

--- sim_moran_imm/s_coef_sim_moran_imm.r
+++ sim_moran_imm/s_coef_sim_moran_imm.r
@@ -2,17 +2,17 @@
 
 # (C) Copyright 2022 Sur Herrera Paredes
 # This file is part of This program.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with This program.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -21,18 +21,18 @@ library(argparser)
 process_arguments <- function(){
   p <- arg_parser(paste("Calculate selection coefficient (s) per site",
                         "from Moran Process simulations"))
-  
+
   # Positional arguments
-  p <- add_argument(p, "simdir",
-                    help = paste("Directory with simulation output"),
+  p <- add_argument(p, "maf_changes",
+                    help = paste("File with dereived allele changes"),
                     type = "character")
-  
+
   # Optional arguments
   p <- add_argument(p, "--N",
                      help = paste("population size in Moran Process"),
                      type = "numeric",
                      default = 100)
-  p <- add_argument(p, "--T",
+  p <- add_argument(p, "--time",
                     help = paste("Number of Moran process steps"),
                     type = "numeric",
                     default = 100)
@@ -45,47 +45,30 @@ process_arguments <- function(){
                     help = paste("File to write output"),
                     type = "character",
                     default = "s_coef.tsv")
-  
+
   # Read arguments
   cat("Processing arguments...\n")
   args <- parse_args(p)
-  
+
   # Process arguments
   return(args)
 }
 
 args <- process_arguments()
-# args <- list(simdir = "/home/sur/micropopgen/exp/2022/today2/sims/sim_1/",
-#              # maf_changes = "output/maf_changes.tsv",
+# args <- list(maf_changes = "output/maf_changes.tsv",
 #              N = 100,
-#              T = 100,
+#              time = 100,
 #              x_0 = 99,
 #              output = "s_coef.tsv")
 library(tidyverse)
+library(HMVAR)
 
 # Read maf_changes
 Dat <- read_tsv(file.path(args$simdir, "maf_changes.tsv"),
-                          col_types = cols(site_id = col_character()))
+                          col_types = cols(site_id = col_character(),
+                                           pop_id = col_character()))
 
 # Calculate selection coefficient
 cat("Calculating selection coefficient...\n")
-s_coef <- Dat %>%
-  mutate(v0 = args$x_0 / args$N) %>%
-  mutate(v1 = v0 + maf_change) %>%
-  mutate(s = (1 / args$T) * log( (v1 / (1 - v1)) * ((1 - v0) / v0) ))  %>%
-  select(site_id, s) %>%
-  filter(!is.infinite(s))
-
-# Process s coefs. If multiple individuals, do a t-test. Otherwise
-# keep without p-val or variance
-cat("Aggregating selection coefficients...\n")
-s_coef <- s_coef %>%
-  group_by(site_id) %>%
-  summarise(s.mean = mean(s),
-            s.sd = sd(s),
-            npts = length(s),
-            .groups = 'drop') %>%
-  mutate(t.s = s.mean / (s.sd / sqrt(npts))) %>%
-  mutate(pval = 2 * pt(abs(t.s), df = npts - 1, lower.tail = FALSE)) 
-
+s_coef <- s_coefficient(Dat = Dat, aggregate = TRUE, time = args$time)
 write_tsv(s_coef, args$output)
